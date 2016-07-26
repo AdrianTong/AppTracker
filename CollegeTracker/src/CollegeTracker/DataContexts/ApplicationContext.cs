a0c29f5c5ee080f986df52f3d76e9f62bf492017
Kory Watson <korywat@gmail.com>
2016-07-26 09:52:33 -0700

    Update Models

---
+++ CollegeTracker/src/CollegeTracker/DataContexts/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CollegeTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using CollegeTracker.Models.CalendarModels;
+
+namespace CollegeTracker.DataContexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Deadline> Deadlines { get; set; }
+        public DbSet<College> Colleges { get; set; }
+        public DbSet<EntranceTest> EntranceTests { get; set; }
+
+    }
+}
